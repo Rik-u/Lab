81da0d3072f8efb59fcf71d0ddb3dd4e7a52b4f1
Riku Voraniphit <voraniphit.k@ku.th>
2024-03-01 13:46:47 +0700

    Updated Lab9.5/06

--- Lab09.5/06.c
+++ Lab09.5/06.c
@@ -2,21 +2,81 @@
 #include<string.h>
 #include<stdlib.h>
 
-struct Linked_list()
+typedef struct Linked_list {
+    int num;
+    struct Linked_list *next;    
+}Num;
 
-void insert() {
-
+void insert(int number, Num** list) {
+    Num* new_node = (Num*)malloc(sizeof(Num));
+    new_node->num = number;
+    new_node->next = NULL;
+    if (list == NULL) {
+        *list = new_node;
+    }
+    else {
+        Num* current_node = *list;
+        Num* prev_node = NULL;
+        while (current_node != NULL && current_node->num <= number) {
+            prev_node = current_node;
+            current_node = current_node->next;
+        }
+        if (prev_node == NULL) {
+            new_node->next = current_node;
+            *list = new_node;
+        }
+        else {
+            new_node->next = current_node;
+            prev_node->next = new_node;
+        }
+    }
 }
 
-void delete() {
-
+void delete(int number, Num** list) {
+    Num* current_node = *list;
+    Num* prev_node = NULL;
+    while (current_node != NULL && current_node->num == number) {
+        prev_node = current_node;
+        current_node = current_node->next;
+    }
 }
 
-void print() {
+void print(Num* list) {
+    Num* current_node = list;
     printf("[ ");
-
+    while (current_node != NULL) {
+        printf("%d ", current_node->num);
+        current_node = current_node->next;
+    }
+    printf("]\n");
 }
 
 int main() {
+    char input, buf[10];
+    int num;
+    Num* linked_list = NULL;
+
+    while (1) {
+        printf("input> ");
+        fgets(buf, sizeof(buf), stdin);
+        sscanf(buf, " %c %d", &input, &num);
+        
+        switch (input) {
+            case 'p':
+                print(linked_list);
+                break;
+            case 'i':
+                insert(num, &linked_list);
+                break;
+            case 'd':
+                delete(num, &linked_list);
+                break;
+            case 'q':
+                exit(0);
+            default:
+                printf("Invalid input\n");
+                break;
+        }
+    }
 
 }

b20d5da08392451f973758b8254b99cde0a1cdef
Riku Voraniphit <voraniphit.k@ku.th>
2024-03-01 10:24:37 +0700

    Added Lab9.5/05-06

---
+++ Lab09.5/06.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+struct Linked_list()
+
+void insert() {
+
+}
+
+void delete() {
+
+}
+
+void print() {
+    printf("[ ");
+
+}
+
+int main() {
+
+}
